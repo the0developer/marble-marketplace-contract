d09446cc078244e5207ba8d230eddf61219fb11c
rafleberry <kraiglyons0929@gmail.com>
2023-01-23 17:35:02 +0800

    fix: resolve auction by top bidder finished

--- marble-marketplace-contract/src/lib.rs
+++ marble-marketplace-contract/src/lib.rs
@@ -2309,7 +2309,7 @@ impl Contract {
 
         if selected_bid.bidder_id == env::predecessor_account_id() {
             assert!(
-                selected_bid.price.clone() > market_data.reserve_price.clone(),
+                selected_bid.price.0 > market_data.reserve_price.0,
                 "Marble: Your bid price isn't bigger than reserve price."
             );
         }
